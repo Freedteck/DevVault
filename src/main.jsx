f361ee58b82b550ce1addb923774c36334f598b0
Freedteck <freedteck@gmail.com>
2026-02-17 10:30:00 +0000

    refactor: Update import paths to lowercase and remove unused route files

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./pages/Root";
-import Home from "./pages/Home";
-import Discussions from "./pages/Discussions";
-import Questions from "./pages/Questions";
-import Updates from "./pages/Updates";
-import QuestionDetails from "./pages/QuestionDetails";
-import UpdateDetails from "./pages/UpdateDetails";
-import Leaderboard from "./pages/Leaderboard";
-import Profile from "./pages/Profile";
+import Root from "./pages/root";
+import Home from "./pages/home";
+import Discussions from "./pages/discussions";
+import Questions from "./pages/questions";
+import Updates from "./pages/updates";
+import QuestionDetails from "./pages/questionDetails";
+import UpdateDetails from "./pages/updateDetails";
+import Leaderboard from "./pages/leaderboard";
+import Profile from "./pages/profile";
 import WalletContext from "./context/WalletContext";
 
 const router = createBrowserRouter([
